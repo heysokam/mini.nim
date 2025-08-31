b1f7223928a498aedfa0b345738d74e532bbf3de
sOkam <7308253+heysokam@users.noreply.github.com>
2025-08-31 20:52:20 +0200

    new: Parser.Scope entry point skeleton

--- src/mini/passes/scope.c
+++ src/mini/passes/scope.c
@@ -3,7 +3,49 @@
 //:____________________________________________________________
 #include "../passes.h"
 
-void mini_parser_scope (mini_Parser* const P) {
+
+//______________________________________
+// @section Parser.scope: Column Position
+//____________________________
+
+static void mini_parser_scope_columnize (
+  mini_Parser* const P
+) {
   (void)P;
+}
+
+
+//______________________________________
+// @section Parser.scope: Indentation Level
+//____________________________
+
+static void mini_parser_scope_indentate (
+  mini_Parser* const P
+) {
+  (void)P;
+}
+
+
+//______________________________________
+// @section Parser.scope: Scope Identity
+//____________________________
+
+static void mini_parser_scope_identify (
+  mini_Parser* const P
+) {
+  (void)P;
+}
+
+
+//______________________________________
+// @section Parser.scope: Entry Point
+//____________________________
+
+void mini_parser_scope (
+  mini_Parser* const P
+) {
+  mini_parser_scope_columnize(P);
+  mini_parser_scope_indentate(P);
+  mini_parser_scope_identify(P);
 }
 
