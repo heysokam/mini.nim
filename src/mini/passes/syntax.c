343873cdde6215cb3273eafa77e98333554765b2
sOkam <7308253+heysokam@users.noreply.github.com>
2025-09-01 19:36:35 +0200

    new: Skeleton for `Parser.Syntax`

--- src/mini/passes/syntax.c
+++ src/mini/passes/syntax.c
@@ -1,9 +1,44 @@
 //:____________________________________________________________
 //  mini.nim  |  Copyright (C) Ivan Mar (sOkam!)  |  MPL-2.0  :
 //:____________________________________________________________
+#include "../tokenizer.h"
+#include "../parser/errors.h"
 #include "../passes.h"
 
-void mini_parser_syntax (mini_Parser* const P) {
+
+static void mini_parser_syntax_var (
+  mini_Parser* const P
+) {
   (void)P;
+}
+
+static void mini_parser_syntax_proc (
+  mini_Parser* const P
+) {
+  (void)P;
+}
+
+static void mini_parser_syntax_whitespace (
+  mini_Parser* const P
+) {
+  (void)P;
+}
+
+
+void mini_parser_syntax (
+  mini_Parser* const P
+) {
+  while (P->pos < P->buf.len) {
+    mini_Token const tk = P->buf.ptr[P->pos];
+    switch (tk.id) {
+      case mini_token_kw_proc     : mini_parser_syntax_proc(P); break;
+      case mini_token_kw_var      : mini_parser_syntax_var(P); break;
+      case mini_token_wht_space   : /* fall-through */
+      case mini_token_wht_newline : mini_parser_syntax_whitespace(P); break;
+      default                     : mini_parser_error(P, "Unknown TopLevel Token: %02zu:%s", P->pos, mini_token_toString(tk.id)); break;
+    }
+    P->pos += 1;
+  }
+  P->pos = 0;
 }
 
