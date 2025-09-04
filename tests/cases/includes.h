ed8af2f90934a15999d9cd9a1729278742281444
sOkam <7308253+heysokam@users.noreply.github.com>
2025-09-04 03:50:49 +0200

    bld: Fix missing null terminator on all file `#embed`s

--- tests/cases/includes.h
+++ tests/cases/includes.h
@@ -12,69 +12,70 @@
 // @section Hello42
 //____________________________
 
+
+// clang-format off
 char const Hello42_nim[] = {
-#embed "./01_hello42.nim"
-};
-
+#embed "./01_hello42.nim" suffix(,)
+0};
 char const Hello42_c[] = {
-#embed "./01_hello42.c"
-};
-
-
+#embed "./01_hello42.c" suffix(,)
+0};
 char const Hello42_h[] = {
-#embed "./01_hello42.h"
-};
+#embed "./01_hello42.h" suffix(,)
+0};
+// clang-format on
+
 
 //______________________________________
 // @section HelloVar
 //____________________________
 
+// clang-format off
 char const HelloVar_nim[] = {
-#embed "./02_helloVar.nim"
-};
-
+#embed "./02_helloVar.nim" suffix(,)
+0};
 char const HelloVar_c[] = {
-#embed "./02_helloVar.c"
-};
-
-
+#embed "./02_helloVar.c" suffix(,)
+0};
 char const HelloVar_h[] = {
-#embed "./02_helloVar.h"
-};
+#embed "./02_helloVar.h" suffix(,)
+0};
+// clang-format on
+
 
 //______________________________________
 // @section HelloVarStatement
 //____________________________
 
+// clang-format off
 char const HelloVarStatement_nim[] = {
-#embed "./03_helloVarStatement.nim"
-};
-
+#embed "./03_helloVarStatement.nim" suffix(,)
+0};
 char const HelloVarStatement_c[] = {
-#embed "./03_helloVarStatement.c"
-};
-
-
+#embed "./03_helloVarStatement.c" suffix(,)
+0};
 char const HelloVarStatement_h[] = {
-#embed "./03_helloVarStatement.h"
-};
+#embed "./03_helloVarStatement.h" suffix(,)
+0};
+// clang-format on
+
 
 //______________________________________
 // @section HelloIndentation
 //____________________________
 
+// clang-format off
 char const HelloIndentation_nim[] = {
-#embed "./04_helloIndentation.nim"
-};
-
+#embed "./04_helloIndentation.nim" suffix(,)
+0};
 char const HelloIndentation_c[] = {
-#embed "./04_helloIndentation.c"
-};
-
-
+#embed "./04_helloIndentation.c" suffix(,)
+0};
 char const HelloIndentation_h[] = {
-#embed "./04_helloIndentation.h"
-};
+#embed "./04_helloIndentation.h" suffix(,)
+0};
+// clang-format on
+
 
 #pragma GCC diagnostic pop
 #endif  // H_tests_cases_includes
